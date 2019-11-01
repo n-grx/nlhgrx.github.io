7f09a60a89f8330bf851584e2d329d0ca3bc3bd2
nickhonline <n.harris@outlook.com>
2019-11-01 17:18:46 +0000

    ytdc

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      projects: [],
-      tasks: []
-    };
-  }
+  state = {
+    isLoading: true,
+    projects: [],
+    tasks: []
+  };
 
   componentDidMount() {
     const myHeaders = new Headers({
@@ -26,46 +23,46 @@ class App extends Component {
       .then(data => {
         this.setState({ projects: data.projects, tasks: data.tasks });
       });
-
-    // =============
-
-    let orderedData = [];
-
-    this.state.projects.forEach(project => {
-      this.state.tasks.filter(task => {
-        if (task.projects === project.name) {
-          orderedData.push(task);
-        }
-      });
-    });
-
-    let taskMeta = [];
-
-    this.state.projects.forEach((project, idx) => {
-      this.state.tasks.filter(task => {
-        if (task.projects === project.name) {
-          taskMeta.push([task.id, idx + 1]);
-        }
-      });
-    });
-
-    let calculateAgeScore = taskId => {
-      let selectedTask = this.state.tasks.filter(task => {
-        return task.id === taskId;
-      });
-
-      let now = new Date();
-      let then = new Date(selectedTask.created * 1000);
-      let timeSince = now - then;
-      return timeSince;
-    };
-
-    console.log("306158's age is:" + calculateAgeScore(306185));
-
-    // =============
   }
 
+  calculateTaskAge = taskCreated => {
+    let age = Math.floor(
+      (new Date() - new Date(taskCreated)) / (1000 * 60 * 60 * 24)
+    );
+    let score;
+    if (age > 60) {
+      score = 3;
+    } else if (age > 10) {
+      score = 2;
+    } else if (age > 7) {
+      score = 1;
+    } else {
+      score = 0;
+    }
+    return score;
+  };
+
+  calculateProjectScore = task => {};
+
+  orderData = tasks => {
+    let rawData = [];
+
+    tasks.map(task => {
+      let totalScore = this.calculateTaskAge(task.created);
+      let newData = [task, totalScore];
+      rawData.push(newData);
+    });
+
+    return rawData.sort(function(a, b) {
+      return a[1] - b[1];
+    });
+  };
+
   render() {
+    this.state.tasks.length > 0
+      ? console.log(this.orderData(this.state.tasks))
+      : console.log('First load.');
+
     return (
       <div className="app">
         <h1>Today's tasks</h1>
@@ -78,15 +75,15 @@ class App extends Component {
           {this.state.tasks.length < 1 ? (
             <li>No Data available</li>
           ) : (
-            this.state.tasks.map(task => (
-              <li className="task">
+            this.state.tasks.map((task, idx) => (
+              <li className="task" key={idx}>
                 <ul>
                   <li>{task.value}</li>
                 </ul>
                 <ul className="label">
                   <li className="project">{task.projects}</li>
                   <li className="created">
-                    {new Date(task.created * 1000).toDateString()}
+                    {new Date(task.created).toLocaleDateString()}
                   </li>
                 </ul>
               </li>
